0e61e4aaa9084347bd2f19922b8cf7f55687c66e
victovito <victoreosr@gmail.com>
2023-08-27 23:11:56 -0300

    build path on server updated

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+const buildPath = __dirname + "/client/build";
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/src/index.html"));
+    res.sendFile(path.join(buildPath, "/index.html"));
 });
-app.use(express.static(__dirname + "/src"));
+app.use(express.static(__dirname + "/client/build"));
 
 app.listen(8080, () => {
     console.log("Listening on port 8080...");
